a47508c92e621f72494be141c0fb9cfe897696eb
fernandala <fernandalassis21@gmail.com>
2020-01-23 09:49:07 -0300

    removendo redundancia

--- View/MontaFile/MontaFileView.php
+++ View/MontaFile/MontaFileView.php
@@ -1,19 +1,9 @@
 
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <script src="../../Resources/jquery/jquery-1.10.1.min.js"></script>
-        <script src="../../Resources/bootstrap/js/popper.min.js"></script>
-        <script src="../../Resources/bootstrap/js/bootstrap.min.js"></script>
-        <!-- Bootstrap core CSS -->
-        <link href="../../Resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="../../Resources/bootstrap/css/dashboard.css" rel="stylesheet">
-        <script src="../../Resources/swal/dist/sweetalert.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="../../Resources/swal/dist/sweetalert.css">          
         <title>Gerar Arquivos - Sisto</title>
         <script src="../../View/MontaFile/JavaScript/MontaFileView.js?rdm=<?php echo V; ?>"></script>        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     </head>
     <body>
         <?php include_once "../../View/MenuPrincipal/Cabecalho.php"; ?>
