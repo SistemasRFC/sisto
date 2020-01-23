2fd6fd1b8d62008370d039511c762c7a5d075763
rafaelfcar <rafaelfcarneiro@gmail.com>
2020-01-23 09:39:31 -0300

    Add files via upload

--- View/Scripts.php
+++ View/Scripts.php
@@ -1,19 +1,24 @@
+<?php
+DEFINE('V', time());
+//DEFINE('V', 1);
+?>
 <html>
         <script src="../../Resources/jquery/jquery-1.10.1.min.js"></script>
-        <script src="../../Resources/bootstrap/js/popper.min.js?rdm=<?php echo time();?>"></script>
-        <script src="../../Resources/bootstrap/js/bootstrap.min.js?rdm=<?php echo time();?>"></script>
+        <script src="../../Resources/bootstrap/js/popper.min.js"></script>
+        <script src="../../Resources/bootstrap/js/bootstrap.min.js"></script>
         <!-- jqwidgets -->
         <script type="text/javascript" src="../../Resources/jqx/jqwidgets/jqxcore.js"></script>
         <script type="text/javascript" src="../../Resources/jqx/jqwidgets/jqxdata.js"></script>
         <!-- Bootstrap core CSS -->
-        <link href="../../Resources/bootstrap/css/bootstrap.min.css?rdm=<?php echo time();?>" rel="stylesheet">
+        <link href="../../Resources/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../../Resources/jqx/jqwidgets/styles/jqx.base.css" type="text/css" />        
         <!-- Custom styles for this template -->
-        <link href="../../Resources/bootstrap/css/dashboard.css?rdm=<?php echo time();?>" rel="stylesheet">
-        <link href="../../Resources/css/style.css?rdm=<?php echo time();?>" rel="stylesheet">
+        <link href="../../Resources/bootstrap/css/dashboard.css" rel="stylesheet">
+        <link href="../../Resources/css/style.css?rdm=<?php echo V;?>" rel="stylesheet">
         <script src="../../Resources/swal/dist/sweetalert.min.js"></script>
         <link rel="stylesheet" type="text/css" href="../../Resources/swal/dist/sweetalert.css">         
         <!-- DataTables -->
         <link rel="stylesheet" type="text/css" href="../../Resources/datatable/datatable.css"/>
         <script type="text/javascript" src="../../Resources/datatable/datatable.js"></script>
-        <script type="text/javascript" src="../../Resources/autoComplete/js/autoComplete.js"></script>
+        
 </html>
